00fcf09a3473944812c6a499cede4c9be37f9e1e
DylanDriessen <dylandriessen1@gmail.com>
2018-10-20 17:49:24 +0200

    Is geprobeerd me xlsx ipv csv, ma nog geen resultaat

--- Person_Script.R
+++ Person_Script.R
@@ -1,10 +1,19 @@
 #install.packages("data.table", type="source", dependencies=TRUE)
 #install.packages("qdap")
+#install.packages("readxl")
+#install.packages("xlsx")
 library(data.table)
 library(dplyr)
 library(qdap)
+library(readxl)
+library(xlsx)
 
 getwd()
-persondata = read.csv("Alfresco.csv")
-colsplit2df(persondata,, c("ID", "Name"), ";")
+#persondata = read.csv("Alfresco.csv")
+my_data = read_excel("Alfresco.xlsx")
 #data.table(persondata)
+my_data
+data.table(my_data)
+colsplit2df(my_data,, c("ID", "Identity"), ",")
+#Mss is proberen me gewoon colsplit, echt een hel
+
