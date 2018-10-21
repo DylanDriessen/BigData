263c415e0e41d58f17703ef9df73901826e6b561
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-10-21 14:45:00 +0200

    DataCleanScriptV2

--- DataCleaningScript.R
+++ DataCleaningScript.R
@@ -1,19 +1,22 @@
 
-
+library('readxl')
 setwd("~/R working directory")
-dataSet = data.frame(lapply( read.csv("Alfresco.csv"), as.character), stringsAsFactors = FALSE)
+dataSet = data.frame(lapply( read_excel("Alfresco.xlsx"), as.character), stringsAsFactors = FALSE)
+print(head(dataSet, 4))
+View(dataSet)
 newDataSet <- data.frame(id=numeric(), name=factor())
 
-for (i in 1:nrow(dataSet)) {
+
+for (i in 1:200) {
   
-  columnDelimited <- strsplit(as.character(dataSet[i,1]), ";")
+  columnDelimited <- strsplit(as.character(dataSet[i,1]), ',"')
   properTableDelimited <- columnDelimited[[1]]
-  
+  print(properTableDelimited)
   id <- as.numeric(properTableDelimited[1])
   name <- properTableDelimited[2]
   row <- data.frame(id, name)
   newDataSet <- rbind(newDataSet, row)
+  containsNumber <- FALSE
 }
 
 View(newDataSet)
-View(dataSet)
