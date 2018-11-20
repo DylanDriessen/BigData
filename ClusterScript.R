70e8c3c6e249595e48226f45fedea74260dbde56
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-11-21 00:19:09 +0100

    oke de dataframes zijn te grot

--- ClusterScript.R
+++ ClusterScript.R
@@ -9,6 +9,7 @@ idDataSet <- as.data.frame(dataSet[,1], drop=FALSE)
 names(idDataSet) <- c('id')
 
 idDataSet <- merge(idDataSet, dataSet,by="id")
+idSet1 <- subset(idDataSet, i < 140451)
 
 #For each name give id's
 nameDataSet <- as.data.frame(dataSet[,2], drop=FALSE)
@@ -16,4 +17,9 @@ names(nameDataSet) <- c('name')
 nameDataSet <- unique(nameDataSet)
 
 nameDataSet <- merge(nameDataSet, dataSet, by="name")
+
+
+
+#Voor elke naam alle namen FUCKING HELL DAS TE GROOT, ZELFS ID AL IN 1/4 GEDEELD
+completeDataSet1 <- merge(nameDataSet, idSet1, by="name")
 
