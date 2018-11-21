13a6dc18eee669e1cc4591bf6fe6f9b81e77f730
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-21 15:39:04 +0100

    Voor Maarten

--- ClusterScript.R
+++ ClusterScript.R
@@ -11,7 +11,7 @@ names(idDataSet) <- c('id')
 idDataSet <- merge(idDataSet, dataSet,by="id")
 
 #Gebruik kleiner aantal documenten
-idSet1 <- subset(idDataSet, i < 140451)
+idSet1 <- subset(idDataSet, id < 140451)
 
 #For each name give id's
 nameDataSet <- as.data.frame(dataSet[,2], drop=FALSE)
@@ -21,15 +21,21 @@ nameDataSet <- unique(nameDataSet)
 nameDataSet <- merge(nameDataSet, dataSet, by="name")
 
 #Voor elke naam alle namen
-completeDataSet1 <- merge(nameDataSet, idSet1, by="name")
+#completeDataSet1 <- merge(nameDataSet, idSet1, by="name")
 
 #Voor specifieke naam alle gelinkte namen
 library(plyr)
-specificDataSet <- merge(subset(nameDataSet, name=="A. Acharya"), idSet1, by="id")
+specificDataSet <- merge(subset(nameDataSet, name=="Jos Snellings"), idSet1, by="id")
 frequencyDataSet <- count(specificDataSet, 'name.y')
 
 #specifieke wordcloud
 library(wordcloud)
-
-wordcloud(frequencyDataSet$name.y, frequencyDataSet$freq, random.order = FALSE)
-
+library(viridis)
+pal = brewer.pal(8,"Dark2")
+#png("wordcloud_test.png", width=12, height=8, units='in', res=300)
+#wordcloud(frequencyDataSet$name.y, frequencyDataSet$freq, random.order = FALSE, max.words = 10)
+#wordcloud(frequencyDataSet$name.y,frequencyDataSet$freq, max.words =100,min.freq=3,scale=c(4,.5), 
+#          random.order = FALSE,rot.per=.5,vfont=c("sans serif","plain"),colors=palette())
+wordcloud(frequencyDataSet$name.y, frequencyDataSet$freq, scale=c(2, .3), min.freq=10,
+                     max.words=Inf, random.order=FALSE, rot.per=.35, random.color = F, use.r.layout=FALSE,
+          colors = pal)
