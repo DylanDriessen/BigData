d990ef25feef60b4aaab9353a57edb36182382cf
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-11-22 00:11:58 +0100

    NetworkStart

--- FrequencyScript.R
+++ FrequencyScript.R
@@ -4,17 +4,16 @@ library(wordcloud)
 library(plyr)
 library(igraph)
 library(RCurl)
+library(zoom)
 
 dataSet <- utf8Data
 names(dataSet) <- c("id", "name")
 #Frequency matrix
-matrix <- dcast(completeDataSet1, name.x~name.y, fun.ag)
+matrix <- dcast(completeDataSet1, name.x~name.y)
 matrix2 <- matrix[,1]
 rownames(matrix) <- matrix[,1]
 matrix$name.x <-NULL
 
-#Normalize
-matrix.normalize <- apply(matrix, )
 
 #Frequency table
 frequencyTable <- count(dataSet, 'name')
@@ -23,10 +22,25 @@ wordcloud(frequencyTable$name, frequencyTable$freq, min.freq=10)
 #Netwerk graph test
 g <- graph.adjacency(as.matrix(matrix), weighted=T, mode = "undirected")
 g <- simplify(g)
-
 V(g)$label <- V(g)$name
 V(g)$degree <- degree(g)
 
-set.seed(3952)
-layout1 <- layout.fruchterman.reingold(g)
+set.seed(6696)
+layout1 <- layout.fruchterman.reingold(g, niter=500)
+V(g)$size=degree(g, mode = "in")/30
+V(g)$color <- "orange"
+plot(g, layout=layout1, rescale = FALSE)
+zm()
+#Better layout
+
+
+
+#V(g)$label.cex <- 2.2 * V(g)$degree / max(V(g)$degree)+ .2
+V(g)$frame.color <- NA
+egam <- (log(E(g)$weight)+.4) / max(log(E(g)$weight)+.4)
+E(g)$color <- rgb(.5, .5, 0, egam)
+E(g)$width <- egam
+# plot the graph in layout1
 plot(g, layout=layout1)
+
+
