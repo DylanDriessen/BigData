0713539f29ef3a099fea21c08fada1569ec705df
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-11-21 22:45:13 +0100

    Netwerk graph

--- FrequencyScript.R
+++ FrequencyScript.R
@@ -1,5 +1,32 @@
 library(data.table)
+library(tm)
+library(wordcloud)
+library(plyr)
+library(igraph)
+library(RCurl)
 
-matrix <- dcast(completeDataSet1, name.x~name.y)
+dataSet <- utf8Data
+names(dataSet) <- c("id", "name")
+#Frequency matrix
+matrix <- dcast(completeDataSet1, name.x~name.y, fun.ag)
+matrix2 <- matrix[,1]
+rownames(matrix) <- matrix[,1]
+matrix$name.x <-NULL
 
-test <- apply(matrix, 2, normalize)
+#Normalize
+matrix.normalize <- apply(matrix, )
+
+#Frequency table
+frequencyTable <- count(dataSet, 'name')
+wordcloud(frequencyTable$name, frequencyTable$freq, min.freq=10)
+
+#Netwerk graph test
+g <- graph.adjacency(as.matrix(matrix), weighted=T, mode = "undirected")
+g <- simplify(g)
+
+V(g)$label <- V(g)$name
+V(g)$degree <- degree(g)
+
+set.seed(3952)
+layout1 <- layout.fruchterman.reingold(g)
+plot(g, layout=layout1)
