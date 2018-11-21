d1c16a6fcbe5f41e2af2f7dac7a5d2852f79d23c
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-22 00:27:26 +0100

    Small changes

--- FrequencyScript.R
+++ FrequencyScript.R
@@ -4,19 +4,24 @@ library(wordcloud)
 library(plyr)
 library(igraph)
 library(RCurl)
+library(zoom)
 
 dataSet <- utf8Data
 names(dataSet) <- c("id", "name")
 #Frequency matrix
-matrix <- dcast(completeDataSet1, name.x~name.y, fun.ag)
+matrix <- dcast(completeDataSet1, name.x~name.y)
 matrix2 <- matrix[,1]
 rownames(matrix) <- matrix[,1]
 matrix$name.x <-NULL
+distance <- netdistance(matrix)
 
 #Normalize
-matrix.normalize <- apply(matrix, )
+#matrix.normalize <- apply(matrix, )
 
 #Frequency table
+library(wordcloud)
+library(viridis)
+pal = brewer.pal(9,"BuGn")
 frequencyTable <- count(dataSet, 'name')
 wordcloud(frequencyTable$name, frequencyTable$freq, min.freq=10)
 
@@ -27,6 +32,16 @@ g <- simplify(g)
 V(g)$label <- V(g)$name
 V(g)$degree <- degree(g)
 
-set.seed(3952)
+set.seed(4)
 layout1 <- layout.fruchterman.reingold(g)
+
+V(g)$label.cex <- 2.2 * V(g)$degree / max(V(g)$degree)+ .2
+V(g)$label.color <- rgb(0, 0, .2, .8)
+V(g)$frame.color <- NA
+egam <- (log(E(g)$weight)+.4) / max(log(E(g)$weight)+.4)
+E(g)$color <- rgb(.5, .5, 0, egam)
+E(g)$width <- egam
+# plot the graph in layout1
 plot(g, layout=layout1)
+#zoom
+zm()
