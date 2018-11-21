8a00620ad969ae85532e99a1df388237c22caf9a
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-21 15:46:56 +0100

    Merge Fixed2

--- ClusterScript.R
+++ ClusterScript.R
@@ -11,11 +11,8 @@ names(idDataSet) <- c('id')
 idDataSet <- merge(idDataSet, dataSet,by="id")
 
 #Gebruik kleiner aantal documenten
-<<<<<<< HEAD
-idSet1 <- subset(idDataSet, id < 140451)
-=======
+
 idSet1 <- subset(idDataSet, id < 3119)
->>>>>>> 72c1c0869e5e878e17dc3ee23806384b90d8aba8
 
 #For each name give id's
 nameDataSet <- as.data.frame(dataSet[,2], drop=FALSE)
@@ -25,11 +22,7 @@ nameDataSet <- unique(nameDataSet)
 nameDataSet <- merge(nameDataSet, dataSet, by="name")
 
 #Voor elke naam alle namen
-<<<<<<< HEAD
-#completeDataSet1 <- merge(nameDataSet, idSet1, by="name")
-=======
 completeDataSet1 <- merge(nameDataSet, idSet1, by="id")
->>>>>>> 72c1c0869e5e878e17dc3ee23806384b90d8aba8
 
 #Voor specifieke naam alle gelinkte namen
 library(plyr)
@@ -38,7 +31,6 @@ frequencyDataSet <- count(specificDataSet, 'name.y')
 
 #specifieke wordcloud
 library(wordcloud)
-<<<<<<< HEAD
 library(viridis)
 pal = brewer.pal(8,"Dark2")
 #png("wordcloud_test.png", width=12, height=8, units='in', res=300)
@@ -48,13 +40,9 @@ pal = brewer.pal(8,"Dark2")
 wordcloud(frequencyDataSet$name.y, frequencyDataSet$freq, scale=c(2, .3), min.freq=10,
                      max.words=Inf, random.order=FALSE, rot.per=.35, random.color = F, use.r.layout=FALSE,
           colors = pal)
-=======
-
-wordcloud(frequencyDataSet$name.y, frequencyDataSet$freq, random.order = FALSE)
 
 #Cluster pogin 1
 namesFreqency <- count(dataSet, 'name')
 
 #Corpus
 corpus = tm:Corpus(tm::VectorSource(subset()))
->>>>>>> 72c1c0869e5e878e17dc3ee23806384b90d8aba8
