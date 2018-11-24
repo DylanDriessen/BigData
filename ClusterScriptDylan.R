02e6bccf4f4e498c74c90c728b662ed0ee8b2092
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-24 19:06:04 +0100

    Work

--- ClusterScriptDylan.R
+++ ClusterScriptDylan.R
@@ -4,9 +4,11 @@ library(wordcloud)
 library(viridis)
 
 dataSet2 <- utf8Data
+#Te kiezen naam
+name <- 'Laurent Morlet'
 
 #Geeft alle unieke ID's van de naam ".."
-nameSetUnique <- unique(dataSet2[dataSet2$entity=='John Powell',])
+nameSetUnique <- unique(dataSet2[dataSet2$entity==name,])
 #View(nameSet)
 
 #Geeft alle unieke namen van personen verbonden met persoon hierboven in nameSet PER id
@@ -22,14 +24,15 @@ par(mfrow=c(1,1))
 pal = brewer.pal(8,"Dark2")
 set.seed(1234)
 png("wordcloudUnique.png", width=1280,height=800)
-wordcloud(frequencyDataSetUnique$entity.y, frequencyDataSetUnique$freq, min.freq=2,scale=c(8, .5),
+wordcloud(frequencyDataSetUnique$entity.y, frequencyDataSetUnique$freq, min.freq=2,scale=c(8, 2),
           max.words=Inf, random.order=FALSE, rot.per=.15,
           colors = pal)
 dev.off()
 
 #Nu alles geven, dus niet uniek
+
 #Geeft alle ID's van de naam ".."
-nameSet <- (dataSet2[dataSet2$entity=='John Powell',])
+nameSet <- (dataSet2[dataSet2$entity==name,])
 View(nameSet)
 
 #Geeft alle namen van personen verbonden met persoon hierboven in nameSet PER id
@@ -45,7 +48,7 @@ par(mfrow=c(1,1))
 pal = brewer.pal(8,"Dark2")
 set.seed(1234)
 png("wordcloud.png", width=1280,height=800)
-wordcloud(frequencyDataSet2$entity.y, frequencyDataSet2$freq, min.freq=2,scale=c(8, .5),
+wordcloud(frequencyDataSet2$entity.y, frequencyDataSet2$freq, min.freq=2,scale=c(8, 2),
           max.words=Inf, random.order=FALSE, rot.per=.15,
           colors = pal)
 dev.off()
