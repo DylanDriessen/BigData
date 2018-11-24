59221d22257aa3020304b43566fa84532edfcdd1
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-24 18:06:48 +0100

    Unieke wordcloud + niet unieke wordcloud -> opgeslagen als afbeeldingen

--- ClusterScriptDylan.R
+++ ClusterScriptDylan.R
@@ -1,24 +1,53 @@
 library(stats)
+library(plyr)
+library(wordcloud)
+library(viridis)
+
 dataSet2 <- utf8Data
 
-#Geeft alle unieke ID's van de naam Rob
-nameSet <- unique(dataSet2[dataSet2$entity=='Kevin Cochrane',])
-View(nameSet)
+#Geeft alle unieke ID's van de naam ".."
+nameSetUnique <- unique(dataSet2[dataSet2$entity=='John Powell',])
+#View(nameSet)
 
 #Geeft alle unieke namen van personen verbonden met persoon hierboven in nameSet PER id
-nameDataSet <- unique(merge(nameSet, dataSet2, by="id"))
+nameDataSetUnique <- unique(merge(nameSetUnique, dataSet2, by="id"))
+#View(nameDataSet)
+
+#Tel frequentie van naam
+frequencyDataSetUnique <- count(nameDataSetUnique, 'entity.y')
+#View(frequencyDataSet2)
+
+#Wordcloud
+par(mfrow=c(1,1))
+pal = brewer.pal(8,"Dark2")
+set.seed(1234)
+png("wordcloudUnique.png", width=1280,height=800)
+wordcloud(frequencyDataSetUnique$entity.y, frequencyDataSetUnique$freq, min.freq=2,scale=c(8, .5),
+          max.words=Inf, random.order=FALSE, rot.per=.15,
+          colors = pal)
+dev.off()
+
+#Nu alles geven, dus niet uniek
+#Geeft alle ID's van de naam ".."
+nameSet <- (dataSet2[dataSet2$entity=='John Powell',])
+View(nameSet)
+
+#Geeft alle namen van personen verbonden met persoon hierboven in nameSet PER id
+nameDataSet <- (merge(nameSet, dataSet2, by="id"))
 View(nameDataSet)
 
 #Tel frequentie van naam
-library(plyr)
 frequencyDataSet2 <- count(nameDataSet, 'entity.y')
 View(frequencyDataSet2)
 
 #Wordcloud
-library(wordcloud)
-library(viridis)
+par(mfrow=c(1,1))
 pal = brewer.pal(8,"Dark2")
 set.seed(1234)
-wordcloud(frequencyDataSet2$entity.y, frequencyDataSet2$freq, scale=c(2, .5), min.freq=2,
+png("wordcloud.png", width=1280,height=800)
+wordcloud(frequencyDataSet2$entity.y, frequencyDataSet2$freq, min.freq=2,scale=c(8, .5),
           max.words=Inf, random.order=FALSE, rot.per=.15,
           colors = pal)
+dev.off()
+  
+  
