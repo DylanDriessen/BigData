f1a67cb091109be4a65ab0768f98481815f29d03
DylanDriessen <dylan.driessen@student.ucll.be>
2018-11-28 22:38:53 +0100

    Small cluster with kmeans

--- CorpusScriptDylan.R
+++ CorpusScriptDylan.R
@@ -3,6 +3,10 @@ library(data.table)
 library(doParallel)
 library(dplyr)
 library(netmeta)
+library(proxy)
+library(skmeans)
+library(kmeans)
+library(stats)
 
 #corpus <- Corpus(VectorSource(dataSet2))
 #print(corpus)
@@ -50,6 +54,36 @@ trial2<-dcast(sampleDataSet3, entity.x ~ entity.y)
 trial2$entity.x <-NULL
 distance <- netdistance(trial2)
 proc.time()[3]-ptm5[3]
+
+#Verder op maarten zijn script
+corpusDylan <- tm::Corpus(tm::VectorSource(documentNamesString))
+corpus.cleaned <- tm::tm_map(corpusDylan, tm::stripWhitespace)  
+tdm <- tm::DocumentTermMatrix(corpus.cleaned) 
+tdm.tfidf <- tm::weightTfIdf(tdm)
+tdm.tfidf <- tm::removeSparseTerms(tdm.tfidf, 0.999) 
+tfidf.matrix <- as.matrix(tdm.tfidf) 
+dist.matrix = proxy::dist(tfidf.matrix, method = "cosine")
+clustering.kmeans <- kmeans(tfidf.matrix, 5)
+
+master.cluster <- clustering.kmeans$cluster 
+names(stacked.clustering) <- 1:length(master.cluster) 
+for (cluster in unique(master.cluster)) { 
+  indexes = which(master.cluster == cluster, arr.ind = TRUE) 
+  slave1.votes <- table(slave.hierarchical[indexes]) 
+  slave1.maxcount <- names(slave1.votes)[which.max(slave1.votes)]   
+  slave1.indexes = which(slave.hierarchical == slave1.maxcount, arr.ind = TRUE) 
+  slave2.votes <- table(slave.dbscan[indexes]) 
+  slave2.maxcount <- names(slave2.votes)[which.max(slave2.votes)]   
+  stacked.clustering[indexes] <- slave2.maxcount 
+}
+
+points <- cmdscale(dist.matrix, k = 2) 
+palette <- colorspace::diverge_hcl(truth.K) # Creating a color palette 
+previous.par <- par(mfrow=c(2,2), mar = rep(1.5, 4)) 
+
+plot(points, main = 'K-Means clustering', col = as.factor(master.cluster), 
+     mai = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), 
+     xaxt = 'n', yaxt = 'n', xlab = '', ylab = '') 
 
 #proc.time -> 25.19 + Error vector size! Merge absoluut niet gebruiken dus!
 #ptm2 <- proc.time()
