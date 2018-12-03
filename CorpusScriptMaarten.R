8a2f1ca7eb22041f7619b407c321a0de3eb32e4e
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-12-03 20:07:01 +0100

    Cleaning fix

--- CorpusScriptMaarten.R
+++ CorpusScriptMaarten.R
@@ -1,6 +1,7 @@
 library(tm)
 library(doParallel)
 library(zoom)
+library(skmeans)
 
 uniqueID <- unique(cleanUtf8Data$id)
 documentNamesString <- NULL
@@ -12,8 +13,8 @@ documentNamerow <- data.frame(names=factor())
 ptm <- proc.time()
 for(i in 1:NROW(uniqueID)){
   
-  test <- cleanUtf8Data[cleanUtf8Data$id == uniqueID[i],]
-  documentNamerow <- paste(test$name, collapse = "__")
+  idSet <- cleanUtf8Data[cleanUtf8Data$id == uniqueID[i],]
+  documentNamerow <- paste(idSet$entity, collapse = " ")
   
   documentNamesString <- rbind(documentNamesString, documentNamerow)
   idNameSet <- data.frame(name=factor())
@@ -24,65 +25,3 @@ proc.time()[3]-ptm[3]
 #Corpus maken
 corpus <- Corpus(VectorSource(documentNamesString))
 
-#DTM maken
-dtm <- DocumentTermMatrix(corpus)
-dtmMatrix <- as.matrix(dtm)
-
-#TDM maken
-tdm <- TermDocumentMatrix(corpus)
-tdmMatrix <- as.matrix(tdm)
-
-print(corpus)
-
-
-#library(wordcloud)
-#wordcloud(corpus, min.freq=10, colors=brewer.pal(8,"Set2"), random.order = FALSE, rot.per = 0.30)
-#comparison.cloud(dtmMatrix, max.words = 100, random.order = FALSE)
-
-
-#Hierarchical clustering
-tdm.sparse <- removeSparseTerms(tdm, sparse = 0.98)
-tdmSparseMatrix <- as.matrix(tdm.sparse)
-
-distMatrix <- dist(scale(tdmSparseMatrix))
-tdm.fit <- hclust(distMatrix, method = "ward.D")
-
-plot(tdm.fit, cex=0.9, hang=-1, main = "Cluster Diagram")
-rect.hclust(tdm.fit, k=5)
-zm()
-tdm.groups <- cutree(tdm.fit, k=5)
-
-
-#SKmeans test
-library(skmeans)
-library(cluster)
-
-distTdm <- dist(tdmMatrix)
-skfit <- skmeans(as.matrix(distTdm), 5)
-clusplot(as.matrix(distTdm), skfit$cluster, color = T, labels = 2)
-zm()
-
-
-
-
-
-
-
-
-
-
-#parLapply test
-library(parallel)
-
-parLapply(c1, 1:NROW(uniqueID), function(i, cleanUtf8Data, idNameSet, documentNamesString, documentNamerow, uniqueID){
-  for(j in 1:nrow(cleanUtf8Data)){
-    if(uniqueID[i] == cleanUtf8Data[j,1]){
-      spaceGone <- gsub(" ", ".", cleanUtf8Data[j,2])
-      spaceGone <- data.frame(spaceGone)
-      names(spaceGone) <- "names"
-      idNameSet <- rbind(idNameSet, spaceGone)
-    }
-  }
-  documentNamerow <- paste(idNameSet$names, collapse = " ")
-  documentNamesString <- rbind(documentNamesString, documentNamerow)
-}, cleanUtf8Data, idNameSet, documentNamesString, documentNamerow, uniqueID)
--- DataCleaningScript2.R
+++ DataCleaningScript2.R
@@ -2,8 +2,6 @@ library('readr')
 library(parallel)
 library(tm)
 
-nr_clusters <- detectCores() - 1
-c1 <- makeCluster(nr_clusters)
 
 utf8Data <-read.table("Alfresco_EN_PDF__Persons_cln.utf8",
            sep=",",
@@ -14,77 +12,9 @@ utf8Data <-read.table("Alfresco_EN_PDF__Persons_cln.utf8",
 
 cleanUtf8Data <<- data.frame(id=numeric(), name=factor())
 
-#With tm package
+#Cleaning with lapply and tm
 ptm <- proc.time()
-for (i in 1:NROW(utf8Data)) {
-  id <- utf8Data[i,1]
-  name <- utf8Data[i,2]
-  cName <- Corpus(VectorSource(name))
-  cName <- tm_map(cName, content_transformer(tolower))
-  cName <- tm_map(cName, removePunctuation)
-  cName <- tm_map(cName, content_transformer(function(x) gsub(x, pattern = '\\-', replacement = "")))
-  
-  name <- content(cName)
-  row <- data.frame(id, name)
-  cleanUtf8Data <- rbind(cleanUtf8Data, row)
-}
-proc.time()[3]-ptm[3]
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Try with foreach DO NOT RUN
-library(foreach)
-library("doParallel")
-
-registerDoParallel(cores=3)
-cleanFuncton <- function(n, utf8Data, cleanUtf8Data){
-  library(tm)
-    id <- utf8Data[n,1]
-    name <- utf8Data[n,2]
-    cName <- Corpus(VectorSource(name))
-    cName <- tm_map(cName, content_transformer(tolower))
-    cName <- tm_map(cName, removePunctuation)
-    cName <- tm_map(cName, content_transformer(function(x) gsub(x, pattern = '\\-', replacement = "")))
-    
-    name <- content(cName)
-    row <- data.frame(id, name)
-    cleanUtf8Data <<- rbind(cleanUtf8Data, row)
-}
-ptm <- proc.time()
-foreach(n=1:NROW(utf8Data)) %dopar% cleanFuncton(n, utf8Data, cleanUtf8Data)
-proc.time()[3]-ptm[3]
-
-#
 utf8Data$entity <- lapply(utf8Data$entity, function(x) tolower(x))
-
-#
-
-#Pogin tot parLapply DO NOT RUN
-ptm <- proc.time()
-parLapply(c1, 1:NROW(utf8Data), function(i, cleanUtf8Data, utf8Data){
-  library(tm)
-  id <- utf8Data[i,1]
-  name <- utf8Data[i,2]
-  
-  cName <- Corpus(VectorSource(name))
-  cName <- tm_map(cName, content_transformer(tolower))
-  cName <- tm_map(cName, removePunctuation)
-  cName <- tm_map(cName, content_transformer(function(x) gsub(x, pattern = '\\-', replacement = "")))
-  
-  name <- content(cName)
-  row <- data.frame(id, name)
-  cleanUtf8Data <- rbind(cleanUtf8Data, row)
-}, cleanUtf8Data = cleanUtf8Data, utf8Data = utf8Data)
+utf8Data$entity <- lapply(utf8Data$entity, function(x) gsub('\\.', "", gsub('\\-', "", gsub(" ", "_", x))))
+cleanUtf8Data <- utf8Data
 proc.time()[3]-ptm[3]
-
-
