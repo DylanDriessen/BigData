7e303c305c43d7b3a7b0139de73e1282b9e5299f
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-12-04 13:39:22 +0100

    count() fix

--- DataCleaningScript2.R
+++ DataCleaningScript2.R
@@ -10,11 +10,11 @@ utf8Data <-read.table("Alfresco_EN_PDF__Persons_cln.utf8",
            stringsAsFactors=FALSE
 )
 
-cleanUtf8Data <<- data.frame(id=numeric(), name=factor())
-
 #Cleaning with lapply and tm
 ptm <- proc.time()
 utf8Data$entity <- lapply(utf8Data$entity, function(x) tolower(x))
 utf8Data$entity <- lapply(utf8Data$entity, function(x) gsub('\\.', "", gsub('\\-', "", gsub(" ", "_", x))))
-cleanUtf8Data <- utf8Data
+
+cleanUtf8Data <- transform(utf8Data, entity=unlist(utf8Data$entity))
 proc.time()[3]-ptm[3]
+names(cleanUtf8Data) <- c("id", "name")
