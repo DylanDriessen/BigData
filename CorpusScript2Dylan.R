a4864b4d78b2653cfe0fefd45aa043529b63570c
DylanDriessen <dylan.driessen@student.ucll.be>
2018-12-04 21:02:35 +0100

    last commit

--- CorpusScript2Dylan.R
+++ CorpusScript2Dylan.R
@@ -4,36 +4,24 @@ library(dplyr)
 library(slam)
 library(skmeans)
 #Source Data
-NewTable <- summarise(group_by(cleanUtf8Data,id,name),count = n())
-NewTable$id <- as.factor(NewTable$id)
-NewTable$name <- as.factor(NewTable$name)
-as.numeric(NewTable$id)
-as.numeric(NewTable$name)
-
+FrequencyCountTable <- summarise(group_by(cleanUtf8Data,id,name),count = n())
+FrequencyCountTable$id <- as.factor(FrequencyCountTable$id)
+FrequencyCountTable$name <- as.factor(FrequencyCountTable$name)
+as.numeric(FrequencyCountTable$id)
+as.numeric(FrequencyCountTable$name)
+summary(FrequencyCountTable)
 #Simple triplet Matrix
-New2Table <- data.frame(unclass(NewTable))
-#library(SnowballC)
-i = c(NewTable$id)
-j = c(NewTable$name)
-v = c(NewTable$count)
+i = c(FrequencyCountTable$id)
+j = c(FrequencyCountTable$name)
+v = c(FrequencyCountTable$count)
 xx = simple_triplet_matrix(i,j,v)
-xx
-
-q <- as.numeric(levels(New2Table$id))
-
 
 #skmeans
 set.seed(1234)
-skmeans <- skmeans(xx, 5)
-
-#Overbodige nest momenteel
-NewTable <- cleanUtf8Data %>%
-  count(NewTable$id, NewTable$name) %>%
-  ungroup()
-total_entities <- NewTable %>% 
-  group_by(NewTable$id) %>% 
-  summarize(total = sum(n))
-NewTable <- left_join(NewTable, total_entities)
-NewTable
-CountTable <- count(cleanUtf8Data, c(cleanUtf8Data$id, cleanUtf8Data$name))
-cleanUtf8Data %>% group_by(id, name) %>% count(cleanUtf8Data, c('id', 'name'))
+skmeans_xdist(xx, y = NULL)
+kfit <- skmeans(xx, 5)
+kfit$value
+require("cluster")
+plot(silhouette(kfit))
+library(cluster)
+clusplot(xx, kfit$cluster, color=T, shade=T, labels=2, lines=0)
